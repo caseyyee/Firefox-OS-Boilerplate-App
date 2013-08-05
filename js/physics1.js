d5ae2c9665787602d42fffad4d597eea9d73393d
Casey Yee <casey@caseyyee.com>
2013-08-05 14:16:07 -0700

    Box2D DOM physics

--- js/physics1.js
+++ js/physics1.js
@@ -16,35 +16,54 @@ var SCALE = 30;
 var stage, world;
 var stageSize;
 
-var canvas = document.getElementById("Physics1Canvas");
+var canvas = document.getElementById("Physics1Canvas");		// canvas element for physics
+var container = document.getElementById("Physics1");		// dom element for physics
 
 function init() {
 	stage = new createjs.Stage(canvas);
-
+	
 	stageSize = {
-		width: canvas.width,
-		height: canvas.height
+		width: container.offsetWidth,
+		height: container.offsetHeight
 	}
+	
 	setupPhysics();
 
-	stage.onMouseDown = createRoundBody;
+	//stage.onMouseDown = createRoundBody;
+	container.addEventListener("click", createRoundBody)
 
 	createjs.Ticker.addListener(this);	
-	createjs.Ticker.setFPS(60);	
+	createjs.Ticker.setFPS(30);	
 	createjs.Ticker.useRAF = true;
 };
 
 function createRoundBody() {
+	var body = {
+		x: Math.random()*stageSize.width,	// randomly on X-axis
+		y: 0,								// from the top @ 0
+		diam: Math.random()*50
+	}
+
 	var fixDef  = new box2d.b2FixtureDef();
 	fixDef.density = 1;
 	fixDef.friction = 0.5;
 	fixDef.restitution = 0.5;
 	var bodyDef = new box2d.b2BodyDef();
 	bodyDef.type = box2d.b2Body.b2_dynamicBody;
-	bodyDef.position.x = Math.random()*stageSize.width / SCALE;
-	bodyDef.position.y = 0
-	fixDef.shape = new box2d.b2CircleShape(Math.random()*50 / SCALE);
-	world.CreateBody(bodyDef).CreateFixture(fixDef);
+	bodyDef.position.x = body.x/SCALE;
+	bodyDef.position.y = body.y;
+	fixDef.shape = new box2d.b2CircleShape(body.diam/SCALE);
+	var b2body = world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+	// create coresponding dom element
+	var elem = document.createElement("div");
+	elem.style["position"] = "absolute";
+	elem.style["width"] = Math.round(body.diam)+"px";
+	elem.style["height"] = Math.round(body.diam)+"px";
+	elem.style["background"] = "red";
+	container.appendChild(elem);
+	// add user data reference to box2d body def
+	b2body.m_userData = { domElem: elem }; 
 }
 
 function setupPhysics() {
@@ -73,11 +92,27 @@ function setupPhysics() {
 
 function tick() {
 	stage.update();
-	world.DrawDebugData();
+	//world.DrawDebugData();
 	world.Step(1/60 // frame rate
 			, 10	// velocity iterations
 			, 10);	// position iterations
 	world.ClearForces();
+
+	updateDom();
+};
+
+function updateDom() {
+	for (var b = world.m_bodyList; b; b = b.m_next) {
+		for (var f = b.m_fixtureList; f; f = f.m_next) {
+			if (f.m_userData) {
+				//console.log(f.m_userData.domElem.offsetWidth);
+				var x = Math.floor((f.m_body.m_xf.position.x * SCALE));
+				var y = Math.floor((f.m_body.m_xf.position.y * SCALE));
+				f.m_userData.domElem.style["left"] = x+"px";
+				f.m_userData.domElem.style["top"] = y+"px";
+			}
+		}
+	}
 };
 
 window.onload = init;
